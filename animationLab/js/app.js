3772879796b1d4ffedd084e853dc408b4bb36769
vcejaeli <vcejaeli@iu.edu>
2020-10-09 21:24:09 -0400

    animationLab

---
+++ animationLab/js/app.js
@@ -0,0 +1,11 @@
+let boxes = document.getElementsByClassName("boxes");
+
+for(let i = 0; < boxes.length; i++) {
+    TweenLite.to(boxes[i], { duration: 2, x: 100, left: 100});
+}
+
+//trying to get this to work :/ 
+//animation is struggling.
+
+
+// let
