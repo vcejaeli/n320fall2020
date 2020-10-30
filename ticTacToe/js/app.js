6ef69faf67520dff210d0f888e09cf42e387b3ea
vcejaeli <vcejaeli@iu.edu>
2020-10-30 18:12:38 -0400

    Tic Tac Toe Game
    Project

---
+++ ticTacToe/js/app.js
@@ -0,0 +1,13 @@
+//elements
+const statusDiv = document.querySelector('.status');
+const blockDivs = document.querySelectorAll('.game-block');
+
+//functionality
+let gameIsLive = true;
+let xTurn = true;
+
+//unsure on how to go about this..
+
+for (const bloxkDiv of blockDivs  ) {
+    console.log(blockDiv);
+}
